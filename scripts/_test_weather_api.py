f593d23a9b71a59109b7a51b26c735ed968353ac
Heng Chuan Lin <linhengchuan@gmail.com>
2023-08-20 11:06:15 +0800

    Add function to check every 2 hours

--- scripts/_test_weather_api.py
+++ scripts/_test_weather_api.py
@@ -6,6 +6,20 @@ two_hours_adr = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
 twenty_four_hours_adr = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast"
 four_days_adr = "https://api.data.gov.sg/v1/environment/4-day-weather-forecast"
 
+response = requests.get(two_hours_adr)
+if response.status_code==200:
+    start_time = response.json().get('items')[0]['valid_period']['start'][:16]
+    end_time = response.json().get('items')[0]['valid_period']['end'][11:16]
+    region_weather = ""
+    ls_region_weather = [i['area'] + ': ' + i['forecast'] + '\n' for i in requests.get(two_hours_adr).json().get('items')[0]['forecasts']]
+    for i in ls_region_weather:
+        region_weather += i 
+    print(
+        f"{start_time}-{end_time}: \n{region_weather}"
+    )
+else:
+    print("Error receiving data.")
+
 response = requests.get(twenty_four_hours_adr)
 if response.status_code==200:
     # print(json.dumps(response.json(), indent=4))
