c4ffc62aa3816df791ce630bedf666fecc31fa93
Heng Chuan Lin <linhengchuan@gmail.com>
2023-08-21 00:27:23 +0800

    Update seaturtle current weather

--- seaturtle/main.py
+++ seaturtle/main.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 )
 
 set_of_chat_id = set()
-MORNING_BRAODCAST_TIME = "22:54"
+MORNING_BRAODCAST_TIME = "23:00"
 TOKEN_ID = "6690176337:AAEFKa9JLcRqzt5N6XBZncXIn1BMbgSFMdI"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -32,23 +32,26 @@ async def weather_auto_end(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 async def weather_now(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    twenty_four_hours_adr = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast"
-    start_time = requests.get(twenty_four_hours_adr).json().get('items')[0]['periods'][0]['time']['start'][:16]
-    end_time = requests.get(twenty_four_hours_adr).json().get('items')[0]['periods'][0]['time']['end'][11:16]
+    two_hours_adr = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
+    response = requests.get(two_hours_adr)
+    start_time = response.json().get('items')[0]['valid_period']['start'][:16]
+    end_time = response.json().get('items')[0]['valid_period']['end'][11:16]
     region_weather = ""
-    for k,v in requests.get(twenty_four_hours_adr).json().get('items')[0]['periods'][0]['regions'].items():
-        region_weather += f"{str(k)}: {v}\n"
+    ls_region_weather = [i['area'] + ': ' + i['forecast'] + '\n' for i in response.json().get('items')[0]['forecasts']]
+    for i in ls_region_weather:
+        region_weather += i 
     text = f"{start_time}-{end_time}: \n{region_weather}"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 async def weather_now_24h(update: Update, context: ContextTypes.DEFAULT_TYPE):
     twenty_four_hours_adr = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast"
+    response = requests.get(twenty_four_hours_adr)
     text = ""
     for i in [0,1,2]:
-        start_time = requests.get(twenty_four_hours_adr).json().get('items')[0]['periods'][i]['time']['start'][:16]
-        end_time = requests.get(twenty_four_hours_adr).json().get('items')[0]['periods'][i]['time']['end'][11:16]
+        start_time = response.json().get('items')[0]['periods'][i]['time']['start'][:16]
+        end_time = response.json().get('items')[0]['periods'][i]['time']['end'][11:16]
         region_weather = ""
-        for k,v in requests.get(twenty_four_hours_adr).json().get('items')[0]['periods'][i]['regions'].items():
+        for k,v in response.json().get('items')[0]['periods'][i]['regions'].items():
             region_weather += f"{str(k)}: {v}\n"
         text += f"{start_time}-{end_time}: \n{region_weather} \n"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
